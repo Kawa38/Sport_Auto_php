e251aeeb40beae251e5959ace89b993207734029
Tournoud Régine <tournoudregine@gmail.com>
2023-04-27 09:29:10 +0200

    Ajout des boucles

--- nwCars.php
+++ nwCars.php
@@ -2,38 +2,24 @@
 require_once "data.php";
 ?>
 
-
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Drive-X</title>
-</head>
-
 <header>
-    <?php require "nwHeader.php" ?>  
+    <?php require "partials/nwHeader.php" ?>  
 </header>
 
 <body>
     <div class="container">
-
         <h1 class="my-5"> <?=$tittle3 ?>   </h1>
-        <?php  for($index=0;$index<count($cars);$index++){
-            include "nwCarCard.php";
-        }
-        ?>
-
+        <div class="row flex-lg-row flex-xl-row flex-column">
+            <?php  for($index=0;$index<count($cars);$index++){
+                include "nwCarCard.php";
+            }
+            ?>
+        </div>
     </div>
 </body>
 
 <footer>
-<?php require "nwFooter.php" ?>  
+<?php require "partials/nwFooter.php" ?>  
 </footer>
 
 </html>
--- nwDrivers.php
+++ nwDrivers.php
@@ -1,22 +1,7 @@
-<?php
-require_once "data.php";
-?>
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Drive-X</title>
-</head>
 
 <header>
-    <?php require "nwHeader.php" ?>  
+    <?php require "partials/nwHeader.php" ?>  
 </header>
 
 <body>
